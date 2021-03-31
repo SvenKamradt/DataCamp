45a1911eb88446adfaa3f1864d933a601bd85341
skamradt <svenkamradt@gmail.com>
2021-03-31 20:41:28 +0100

    Finalized DataCamp

--- COVID.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-covid_vaccination = pd.read_csv("/kaggle/input/covid-world-vaccination-progress/country_vaccinations.csv")
-covid_vaccination.head(10)
